e90f4ff7af8a562cca87b0a9ed1c9881dfb2f747
Vivek <vb79798@gmail.com>
2023-11-19 17:13:03 +0530

    add blogdetails page

--- src/myComponents/BlogDetail.js
+++ src/myComponents/BlogDetail.js
@@ -1,36 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+function BlogDetail() {
 
-function BlogDetail(props) {
-  const [blog, setBlog] = useState(null);
+  const { id } = useParams();
+  const [blog, setBlog] = useState({});
+  console.log(id);
 
   useEffect(() => {
-    // Extract the blog ID from the URL parameter
-    const blogId = props.match.params.id;
-
-    // Fetch the specific blog post using the ID
-    fetch(`https://multibogs.onrender.com/api/blogs/${blogId}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setBlog(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching blog post:', error);
-      });
-  }, [props.match.params.id]);
-
-  if (!blog) {
-    // Loading state or error handling can be added here
-    return <div>Loading...</div>;
-  }
-
+    const fetchBlog = async () => {
+      try {
+        // Fetch the individual blog from the API using the ID
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/blog/${id}`);
+        console.log(response.data);
+        setBlog(response.data);
+      } catch (error) {
+        console.error('Error fetching blog details:', error);
+      }
+    };
+  
+    fetchBlog();
+  }, [id]);
+  
   return (
     <section className="blog-detail">
       <button onClick={() => window.history.back()}>Back</button>
+      <img src={blog.image} className="card-img-top" alt="noimg1" />
       <h2>{blog.title}</h2>
       <p>{blog.content}</p>
     </section>
